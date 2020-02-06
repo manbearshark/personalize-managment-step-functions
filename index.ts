1810dae528a8dc42294a1f76ddd8e589c20b88ee
Krtolica <igorski@a483e721ecb0.ant.amazon.com>
2020-02-06 10:51:30 -0800

    Added personalize states

--- index.ts
+++ index.ts
@@ -7,31 +7,38 @@ class JobPollerStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props: cdk.StackProps = {}) {
         super(scope, id, props);
 
-        const lambdaFn = new lambda.Function(this, 'IncrementFunction', {
+        const lambdaFn = new lambda.Function(this, 'PersonalizeAPIExecutor', {
             code: new lambda.AssetCode('resource/lambda'),
             handler: 'action-executor.handler',
             runtime: lambda.Runtime.NODEJS_10_X,
           });
         
-        const submitJob = new sfn.Task(this, 'Submit Job', {
+        const createDatasetGroupTask = new sfn.Task(this, 'Create Dataset Group', {
+            task: new sfn_tasks.InvokeFunction(lambdaFn)
+        });
+
+        const describeDatasetGroupStatusTask = new sfn.Task(this, 'Describe Dataset Group', {
             task: new sfn_tasks.InvokeFunction(lambdaFn)
         });
       
-        const createDatasetImportJob = new sfn.Pass(this, 'Set Create Dataset Import Job', {
-            result: { value: "createDatasetImportJob" },
+        const setCreateDatasetGroup = new sfn.Pass(this, 'Set Create Dataset Group', {
+            result: { value: { verb: "createDatasetGroup", params: { name: "$.datasetGroupName" } } },
             resultPath: "$.action"
         });
+
+        //TODO: Add Wait for datasetGroup
 
         const successJob = new sfn.Succeed(this, 'Success');
 
         const isComplete = new sfn.Choice(this, 'Job Complete?');
 
         const chain = sfn.Chain
-            .start(submitJob)
-            .next(createDatasetImportJob)
+            .start(setCreateDatasetGroup)
+            .next(createDatasetGroupTask)
+            .next(describeDatasetGroupStatusTask)
             .next(isComplete
                 .when(sfn.Condition.numberEquals('$.count', 5), successJob)
-                .when(sfn.Condition.numberLessThan('$.count', 5), submitJob));
+                .when(sfn.Condition.numberLessThan('$.count', 5), submitJob));  //TODO: Add wait loop state
 
         new sfn.StateMachine(this, 'StateMachine', {
             definition: chain,
